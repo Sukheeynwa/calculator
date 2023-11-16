7187b74f659851cc2396e449a57a354fd762f5c7
Munkhsukh <sukheearch@gmail.com>
2023-11-16 23:01:53 +0800

    made javascript more simple

--- javascript.js
+++ javascript.js
@@ -1,120 +1,76 @@
-function add(a, b) {
-    return a + b;
-};
-
-function subtract(a, b) {
-    return a - b;
-};
-
-function multiply(a, b) {
-    return a * b;
-};
-
-function divide(a, b) {
-        return a / b;
-};
-
-let number = 0;
-let operator = "+";
-let sum = 0;
-
 function operate(a, b, op) {
     if (op === "+") {
-        return add(a, b)
+        return a + b;
     } else if (op === "-") {
-        return subtract(a, b)
+        return a - b;
     } else if (op === "*") {
-        return multiply(a, b)
+        return a * b;
     } else if (op === "/") {
-        return divide(a, b)
-    };
+        return a / b;
+    }
 };
 
-let buttons = [];
-let displayValue = "";
-let sumDisplay = document.querySelector("#sum-display");
-let display = document.querySelector("#display");
+const btnDigit = document.querySelectorAll("#btn-digit");
+console.log(btnDigit);
+const btnOp = document.querySelectorAll("#btn-op");
+console.log(btnOp);
+const currentScreen = document.querySelector("#current-screen");
+const  resultScreen = document.querySelector("#result-screen");
+const btnDot = document.querySelector("#btn-dot");
+const btnEquals = document.querySelector("#btn-equals");
 
-// Нэг бүрчлэн button элементийг дуудалгүйгээр loop хийж 1-9 хүртэлх id-тай button-уудыг дуудан buttons нэртэй Nodelist үүсгэсэн.
-for (let i = 0; i <= 9; i ++) {
-    buttons[i] = document.querySelectorAll(`#btn-${i}`);
-};
+let result = 0;
+let value = "";
+let operator = "+";
 
-// Дуудсан button элементүүд дээрээ event listener нэмэн, displayValue дотор 1-9 хүртэлх тоог store хийсэн.
-for (let i = 0; i <= 9; i ++) {
-    buttons[i][0].addEventListener("click", () => {
-        displayValue += i.toString();
-        display.textContent = displayValue;
-        number = Number(displayValue);
-    });
-};
-
-// Setting num1 value and operator when arithmetic operators button clicked
-let btnAdd = document.querySelector("#btn-add");
-btnAdd.addEventListener("click", () => {
-    sum = operate(sum, number, operator)
-
-    display.textContent = sum;
-    displayValue = ""
-    operator = "+"
-    sumDisplay.textContent = sum + operator;
+btnDigit.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if(operator === "") {
+            value = btn.textContent;
+            result = 0;
+            operator = "+"
+            currentScreen.textContent = value;
+            resultScreen.textContent = result;
+        } else {
+            value += btn.textContent;
+            currentScreen.textContent = value;
+        }
+    })
 });
 
-let btnSubtract = document.querySelector("#btn-subtract");
-btnSubtract.addEventListener("click", () => {
-    sum = operate(sum, number, operator)
+btnOp.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        operator = btn.textContent;
 
-    display.textContent = sum;
-    displayValue = ""
-    operator = "-"
-    sumDisplay.textContent = sum + operator;
+        result = operate(result, Number(value), operator);
+        resultScreen.textContent = result + operator;
+        currentScreen.textContent = result;
+        value = "";
+    })
 });
 
-let btnMultiply = document.querySelector("#btn-multiply");
-btnMultiply.addEventListener("click", () => {
-    sum = operate(sum, number, operator)
-
-    display.textContent = sum;
-    displayValue = ""
-    operator = "*"
-    sumDisplay.textContent = sum + operator;
-});
-
-let btnDivide = document.querySelector("#btn-divide");
-btnDivide.addEventListener("click", () => {
-    sum = operate(sum, number, operator)
-
-    display.textContent = sum;
-    displayValue = ""
-    operator = "/"
-    sumDisplay.textContent = sum + operator;
-});
-
-let btnNotation = document.querySelector("#btn-notation");
-btnNotation.addEventListener("click", () => {
-    displayValue += "."
-    sumDisplay.textContent += "."
-})
-
-// Setting num2 value when = operator clicked, then execute operate function
-let btnEquals = document.querySelector("#btn-equals");
 btnEquals.addEventListener("click", () => {
-    number = Number(displayValue);
-    sum = operate(sum, number, operator)
+    result = operate(result, Number(value), operator);
+    value = "";
 
-
-    display.textContent = sum;
-    sumDisplay.textContent = sum;
-    number = 0;
+    resultScreen.textContent = result;
+    currentScreen.textContent = result;
+    operator = "";
 });
 
 
-let btnClear = document.querySelector("#btn-clear");
-btnClear.addEventListener("click", () => {
-    sumDisplay.textContent = "0";
-    display.textContent = "0";
-    displayValue = ""
+// Pseudocode
 
-    number = 0;
-    sum = 0;
-});
+// Operate function aa hiine
+// Result, value gesen variable zarlana
+// Button daragdah uyd button-s 0-9 hurtelh value awna
+    // Olon orontoi toog awdag baih yostoi
+// Operator button deer darah bolgond operator songogdood operate function ajillaj, operate function-s garsan valueg resulttai tentsuulne 
+
+// result = 0;
+// operator = "+"
+// Btn click => value set
+// 4 clicked => value = 4
+// "-" btn click => result = result + value, operator = "-"
+// "-" btn click => result = 0 + 4, operator = "-"
+
