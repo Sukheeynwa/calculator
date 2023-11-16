fdd982cb6af051809e53ba5af3e08518bfeeca69
unknown <suheeynwa@gmail.com>
2023-11-16 16:05:21 +0800

    modified javascript code

--- javascript.js
+++ javascript.js
@@ -11,12 +11,12 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
-    return a / b;
+        return a / b;
 };
 
-let num1 = 0;
-let num2 = 0;
-let operator = "";
+let number = 0;
+let operator = "+";
+let sum = 0;
 
 function operate(a, b, op) {
     if (op === "+") {
@@ -32,6 +32,7 @@ function operate(a, b, op) {
 
 let buttons = [];
 let displayValue = "";
+let sumDisplay = document.querySelector("#sum-display");
 let display = document.querySelector("#display");
 
 // Нэг бүрчлэн button элементийг дуудалгүйгээр loop хийж 1-9 хүртэлх id-тай button-уудыг дуудан buttons нэртэй Nodelist үүсгэсэн.
@@ -44,52 +45,76 @@ for (let i = 0; i <= 9; i ++) {
     buttons[i][0].addEventListener("click", () => {
         displayValue += i.toString();
         display.textContent = displayValue;
+        number = Number(displayValue);
     });
 };
 
 // Setting num1 value and operator when arithmetic operators button clicked
 let btnAdd = document.querySelector("#btn-add");
 btnAdd.addEventListener("click", () => {
-    num1 = Number(displayValue)
+    sum = operate(sum, number, operator)
+
+    display.textContent = sum;
     displayValue = ""
     operator = "+"
-    console.log(operator)
+    sumDisplay.textContent = sum + operator;
 });
 
 let btnSubtract = document.querySelector("#btn-subtract");
 btnSubtract.addEventListener("click", () => {
-    num1 = Number(displayValue)
+    sum = operate(sum, number, operator)
+
+    display.textContent = sum;
     displayValue = ""
     operator = "-"
-    console.log(operator)
+    sumDisplay.textContent = sum + operator;
 });
 
 let btnMultiply = document.querySelector("#btn-multiply");
 btnMultiply.addEventListener("click", () => {
-    num1 = Number(displayValue)
+    sum = operate(sum, number, operator)
+
+    display.textContent = sum;
     displayValue = ""
     operator = "*"
-    console.log(operator)
+    sumDisplay.textContent = sum + operator;
 });
 
 let btnDivide = document.querySelector("#btn-divide");
 btnDivide.addEventListener("click", () => {
-    num1 = Number(displayValue)
+    sum = operate(sum, number, operator)
+
+    display.textContent = sum;
     displayValue = ""
     operator = "/"
-    console.log(operator)
+    sumDisplay.textContent = sum + operator;
 });
+
+let btnNotation = document.querySelector("#btn-notation");
+btnNotation.addEventListener("click", () => {
+    displayValue += "."
+    sumDisplay.textContent += "."
+})
 
 // Setting num2 value when = operator clicked, then execute operate function
 let btnEquals = document.querySelector("#btn-equals");
 btnEquals.addEventListener("click", () => {
-    num2 = Number(displayValue)
-    display.textContent = operate(num1, num2, operator)
-    displayValue = display.textContent
-})
+    number = Number(displayValue);
+    sum = operate(sum, number, operator)
+
+
+    display.textContent = sum;
+    sumDisplay.textContent = sum;
+    number = 0;
+});
+
 
 let btnClear = document.querySelector("#btn-clear");
 btnClear.addEventListener("click", () => {
+    sumDisplay.textContent = "0";
     display.textContent = "0";
-    displayValue = 0;
-})
+    displayValue = ""
+
+    number = 0;
+    sum = 0;
+});
